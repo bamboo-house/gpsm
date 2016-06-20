96037b410f56d89ffbe93fe3346e8a977138a70f
nguyentrh <tranhanguyen1986@gmail.com>
2016-06-20 21:47:59 +0800

    a

--- util/gutil.h
+++ util/gutil.h
@@ -3,6 +3,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <iostream>
 #include "cutil.h"
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
@@ -21,6 +26,16 @@
     n |= n >> 8;                                    \
     n |= n >> 16;                                   \
     n ++; } while (0) 
+
+#define FOR_LIMIT(i, len)			for(int i=0; i<len; i++)
+
+#define FOR_LIMIT_REV(i, len)		for(int i=len-1; i>=0; i--)
+
+#define FOR_RANGE(i, begin, end)		for(int i=begin; i<end; i++)
+
+#define FOR_RANGE_REV(i, begin, end)	for(int i=end-1; i>=begin; i--)
+
+#define FILL(arr, size, val)	for(int i=0; i< size; i++) arr[i] = val
 
 enum {
 	/* supported copy types */
